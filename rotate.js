d096ac4d8edb20c6fbf34a64c47e8a14b66e8536
pbaker123 <p.baker1@yahoo.com>
2020-08-12 10:26:08 -0500

    Add files via upload

--- rotate.js
+++ rotate.js
@@ -1,26 +1,34 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+const selectors = {
+  base: document.querySelector(".baseContainer"),
+  busp: document.querySelector(".buspContainer"),
+  cust: document.querySelector(".custContainer"),
+  store: document.querySelector(".storeContainer"),
+  baseCover: document.querySelector(".baseCover"),
+  buspCover: document.querySelector(".buspCover"),
+};
+let isDown = false;
 
 container.addEventListener("mousemove", function(event) {
-  const midX = event.pageX - window.innerWidth / 2;
-  const midY = event.pageY - window.innerHeight / 2;
-  const distance = Math.sqrt( (Math.pow(Math.abs(midX),2)) + (Math.pow(Math.abs(midY),2)));
-  const opacity = distance / 400;
-
-  const selectors = {
-    base: document.querySelector(".baseContainer"),
-    busp: document.querySelector(".buspContainer"),
-    cust: document.querySelector(".custContainer"),
-    store: document.querySelector(".storeContainer"),
-    baseCover: document.querySelector(".baseCover"),
-    buspCover: document.querySelector(".buspCover"),
-  };
-  
-  transform(midX, midY, distance, selectors);
-  opacityAdjustment(opacity, selectors);
-  
+  if (isDown === true) {
+    const midX = event.pageX - window.innerWidth / 2;
+    const midY = event.pageY - window.innerHeight / 2;
+    const distance = Math.sqrt( (Math.pow(Math.abs(midX),2)) + (Math.pow(Math.abs(midY),2)));
+    const opacity = distance / 400;
+    transform(midX, midY, distance);
+    opacityAdjustment(opacity);
+  }
 });
 
-function transform(midX, midY, distance, selectors){
+container.addEventListener("mousedown", function() {
+  isDown = true;
+})
+
+container.addEventListener("mouseup", function() {
+  isDown = false;
+})
+
+function transform(midX, midY, distance){
   container.style.transform = "rotateX(" + (midY * .5) + "deg) rotateY(" + (midX * .5) + "deg)"
   if (distance < 250) {
     selectors.base.style.transform = "translateZ(" + (distance * -3) + "px)"
@@ -35,7 +43,7 @@ function transform(midX, midY, distance, selectors){
   }
 };
 
-function opacityAdjustment(opacity, selectors) {
+function opacityAdjustment(opacity) {
   if (opacity < .5) {
     selectors.baseCover.style.opacity = 1 - opacity
     selectors.buspCover.style.opacity = 1 - opacity
