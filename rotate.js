b6f2604cb8782bbfab3a89d3a1bc782ce0f849bc
pbaker123 <p.baker1@yahoo.com>
2020-08-12 09:23:17 -0500

    Add files via upload

---
+++ rotate.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container")
+
+container.addEventListener("mousemove", function(event) {
+  const midX = event.pageX - window.innerWidth / 2;
+  const midY = event.pageY - window.innerHeight / 2;
+  const distance = Math.sqrt( (Math.pow(Math.abs(midX),2)) + (Math.pow(Math.abs(midY),2)));
+  const opacity = distance / 400;
+
+  const selectors = {
+    base: document.querySelector(".baseContainer"),
+    busp: document.querySelector(".buspContainer"),
+    cust: document.querySelector(".custContainer"),
+    store: document.querySelector(".storeContainer"),
+    baseCover: document.querySelector(".baseCover"),
+    buspCover: document.querySelector(".buspCover"),
+  };
+  
+  transform(midX, midY, distance, selectors);
+  opacityAdjustment(opacity, selectors);
+  
+});
+
+function transform(midX, midY, distance, selectors){
+  container.style.transform = "rotateX(" + (midY * .5) + "deg) rotateY(" + (midX * .5) + "deg)"
+  if (distance < 250) {
+    selectors.base.style.transform = "translateZ(" + (distance * -3) + "px)"
+    selectors.busp.style.transform = "translateZ(" + (distance * -1) + "px)"
+    selectors.cust.style.transform = "translateZ(" + (distance) + "px)"
+    selectors.store.style.transform = "translateZ(" + (distance * 3) + "px)"
+  } else {
+    selectors.base.style.transform = "translateZ(" + -750 + "px)"
+    selectors.busp.style.transform = "translateZ(" + -250 + "px)"
+    selectors.cust.style.transform = "translateZ(" + 250 + "px)"
+    selectors.store.style.transform = "translateZ(" + 750 + "px)"
+  }
+};
+
+function opacityAdjustment(opacity, selectors) {
+  if (opacity < .5) {
+    selectors.baseCover.style.opacity = 1 - opacity
+    selectors.buspCover.style.opacity = 1 - opacity
+  } else {
+    selectors.baseCover.style.opacity = .5
+    selectors.buspCover.style.opacity = .5
+  }
+};
